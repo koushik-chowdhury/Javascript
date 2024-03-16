b9fcab20d34092486f19ad59234ff624f099a764
Koushik Chowdhury <csakoushik@gmail.com>
2024-03-16 23:23:07 +0530

    javascript operator and variable

---
+++ javascript basics/variable.js
@@ -0,0 +1,5 @@
+const accountId = 1234
+let accountEmail = "csakoushia@gmail.com"
+var accountPassword = "1234"
+accountCity = "Malda"
+console.table([accountId, accountEmail, accountPassword, accountCity]);
