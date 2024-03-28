75f170c47a6b0544d61b408ba5582afdb21cdcfa
Koushik Chowdhury <csakoushik@gmail.com>
2024-03-28 23:42:32 +0530

    datatype

---
+++ javascript basics/datatype.js
@@ -0,0 +1,17 @@
+// number => 2 to power 53
+// big int
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined
+// symbol => unique
+// object
+
+let name = "koushik"
+let age = 17
+let isLoggedIn = false
+let state;
+
+console.log(typeof "koushik");
+console.log(typeof null);
+console.log(isLoggedIn);
