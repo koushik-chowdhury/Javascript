b18c706ef6964bc3e12f797b6f968aace607cca4
Koushik Chowdhury <csakoushik@gmail.com>
2024-03-29 22:54:51 +0530

    conversion of datatype

---
+++ javascript basics/datatypeConversion.js
@@ -0,0 +1,24 @@
+let score = 33
+console.log(typeof score);
+
+let age = "18"
+console.log(typeof age);
+
+let Myage = Number(age)
+console.log(typeof Myage);
+
+let name = "koushik"
+
+let data = Number(name)
+console.log(typeof data);
+
+// "33" => 33
+// "33ksj" => NaN
+// true => 1; false => 0
+// "" => false
+// "koushik" => true
+
+let loggin = 1
+
+let islogin = Boolean(loggin)
+console.log(typeof islogin);
