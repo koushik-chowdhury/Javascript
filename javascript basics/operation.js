d309cb4a50fa9475f1d393e6553a23a112eae76f
Koushik Chowdhury <csakoushik@gmail.com>
2024-03-31 22:55:03 +0530

    add basic operation

---
+++ javascript basics/operation.js
@@ -0,0 +1,15 @@
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2/2);
+console.log(2**2);
+console.log(2%2);
+
+let Fname = "koushik"
+let Lname = " chowdhury"
+let FullName = fname + lname 
+console.log(FullName);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log(2 + 2 + "1");
