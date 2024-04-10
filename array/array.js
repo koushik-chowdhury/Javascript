fb3fe1601b268f143a4dfdfe73cf11dab724883e
Koushik Chowdhury <csakoushik@gmail.com>
2024-04-10 21:36:31 +0530

    add array

---
+++ array/array.js
@@ -0,0 +1,37 @@
+const myArr = [1,3,4,5,]
+console.log(myArr);
+
+myArr[0] = 20
+console.log(myArr);
+
+// array creation
+
+const array = new Array(1,2,3,4,5,6)
+console.log(array);
+
+console.log("----------------Array methods ----------------");
+
+array.push(39)
+console.log(`Push 39:  ${array}`);
+
+array.pop()
+console.log(array);
+
+array.unshift(2)
+console.log(`Unshift method: ${array}`);
+
+console.log("-----------------------------------");
+
+const newArr = myArr.join()
+console.log(myArr);
+console.log(newArr, typeof newArr)
+
+console.log("-------------SLICE AND SPLICE ----------------");
+
+const arraySlice = array.slice(1, 3)
+console.log("ORIGINAL ARRAY A:", array);
+console.log("Slice Array: ", arraySlice);
+
+const arraySplice = array.splice(1, 3)
+console.log("ORIGINAL ARRAY B:", array);
+console.log("Splice Array:", arraySplice);
