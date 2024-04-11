536558c957ceee4770798d27b11bbaf4b1d3fd4c
Koushik Chowdhury <csakoushik@gmail.com>
2024-04-11 22:21:49 +0530

    updated comparison operator

--- javascript basics/comparison.js
+++ javascript basics/comparison.js
@@ -1,12 +1,12 @@
-// console.log(2 > 1);
-// console.log(2 >= 1);
-// console.log(2 < 1);
-// console.log(2 == 1);
-// console.log(2 != 1);
+console.log(2 > 1);
+console.log(2 >= 1);
+console.log(2 < 1);
+console.log(2 == 1);
+console.log(2 != 1);
 
 
-// console.log("2" > 1);
-// console.log("02" > 1);
+console.log("2" > 1);
+console.log("02" > 1);
 
 console.log(null > 0);
 console.log(null == 0);
@@ -16,6 +16,6 @@ console.log(undefined == 0);
 console.log(undefined > 0);
 console.log(undefined < 0);
 
-// === 
+// === checks dataType
 
 console.log("As the datatypes are not same ","2" === 2);
