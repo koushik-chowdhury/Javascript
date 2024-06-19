b3ae44802096e55e18e94661641537fc8be0763c
Koushik Chowdhury <csakoushik@gmail.com>
2024-06-19 23:30:53 +0530

    modify

--- array/array.js
+++ array/array.js
@@ -1,37 +1,43 @@
-const myArr = [1,3,4,5,]
-console.log(myArr);
+// const myArr = [1,3,4,5,]
+// console.log(myArr);
 
-myArr[0] = 20
-console.log(myArr);
+// myArr[0] = 20
+// console.log(myArr);
 
-// array creation
+// // array creation
 
-const array = new Array(1,2,3,4,5,6)
-console.log(array);
+// const array = new Array(1,2,3,4,5,6)
+// console.log(array);
 
-console.log("----------------Array methods ----------------");
+// console.log("----------------Array methods ----------------");
 
-array.push(39)
-console.log(`Push 39:  ${array}`);
+// array.push(39)
+// console.log(`Push 39:  ${array}`);
 
-array.pop()
-console.log(array);
+// array.pop()
+// console.log(array);
 
-array.unshift(2)
-console.log(`Unshift method: ${array}`);
+// array.unshift(2)
+// console.log(`Unshift method: ${array}`);
 
-console.log("-----------------------------------");
+// console.log("-----------------------------------");
 
-const newArr = myArr.join()
-console.log(myArr);
-console.log(newArr, typeof newArr)
+// const newArr = myArr.join()
+// console.log(myArr);
+// console.log(newArr, typeof newArr)
 
-console.log("-------------SLICE AND SPLICE ----------------");
+// console.log("-------------SLICE AND SPLICE ----------------");
 
-const arraySlice = array.slice(1, 3)
-console.log("ORIGINAL ARRAY A:", array);
-console.log("Slice Array: ", arraySlice);
+// const arraySlice = array.slice(1, 3)
+// console.log("ORIGINAL ARRAY A:", array);
+// console.log("Slice Array: ", arraySlice);
 
-const arraySplice = array.splice(1, 3)
-console.log("ORIGINAL ARRAY B:", array);
-console.log("Splice Array:", arraySplice);
+// const arraySplice = array.splice(1, 3)
+// console.log("ORIGINAL ARRAY B:", array);
+// console.log("Splice Array:", arraySplice);
+
+const myArray = [1, 2, 3, 4, 5]
+
+// method 
+// push(), pop(), unshift(), shift(), includes(), indexof(),
+// slice, splice
