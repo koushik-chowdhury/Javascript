b3d1d8aa7f038224eef1da599703d5ec8691f0a7
Koushik Chowdhury <csakoushik@gmail.com>
2024-07-02 21:28:17 +0530

    array

--- array/array.js
+++ array/array.js
@@ -1,10 +1,6 @@
-
-const myArray = [1, 2, 3, 4, 5]
-// console.log(myArray[0]);
-
-const myHeros = ["Iron Man", "Captain America", "Thor", "Hulk"]
-
-const array = new Array(1,2,3,4,5)
+const myArray = [1, 2, 3, 4, 5];
+const myHeros = ["Iron Man", "Captain America", "Thor", "Hulk"];
+const array = new Array(1, 2, 3, 4, 5);
 
 // Array methods
 
@@ -14,3 +10,19 @@ console.log(myArray);
 myArray.pop()
 console.log(myArray);
 
+// includes(value), indexOf(value) , join()
+
+// slice, splice
+console.log(`----------Slice -----------`);
+
+console.log(`A ${myArray}`);
+const myn1 = myArray.slice(1, 3);
+console.log(myn1);
+console.log(`B ${myArray}`);
+
+console.log(`----------Splice -----------`);
+
+console.log(`A ${myArray}`);
+const myn2 = myArray.splice(1, 3);
+console.log(myn2);
+console.log(`B ${myArray}`);
