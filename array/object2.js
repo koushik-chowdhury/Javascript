deed9bf854b98786a6d83573f18381327e33e5e2
Koushik Chowdhury <csakoushik@gmail.com>
2024-07-03 22:33:02 +0530

    object

---
+++ array/object2.js
@@ -0,0 +1,25 @@
+// singleton
+
+// object literals
+
+// object.create
+
+const jsUser = {
+  name: "Koushik",
+  surname: "Chowdhury",
+  age: 21,
+  email: "koushika@koushik.com",
+  isLoggedIn: false,
+  location: {
+    dist: "Malda",
+    village: "Nageswarpur",
+  },
+};
+
+const print = `${"dist"}`;
+console.log(jsUser.location[print]);
+
+const update = jsUser[`${"email"}`]
+
+jsUser.update = "csakoushik"
+console.log(jsUser["update"]);
