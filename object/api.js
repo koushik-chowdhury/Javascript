8c8cecf9719a7b3b9922e1212c09ecbb883f61e3
Koushik Chowdhury <csakoushik@gmail.com>
2024-07-18 22:28:09 +0530

    object

---
+++ object/api.js
@@ -0,0 +1,18 @@
+// Last lecture of Object
+
+// Destructuring 
+
+const user = {
+    id: 1,
+    firstName: "Koushik",
+    lastName: "Chowdhury",
+    city: "Malda",
+    state: "West Bengal"
+}
+
+
+// Destructuring 
+
+const {id, firstName, lastName, city, state} = user;
+
+console.log(firstName);
---
+++ object/index.js
@@ -0,0 +1,17 @@
+// DEPTH
+
+// singleton -> read more about it from internet
+
+// if we create object using constructor then it will be singleton
+
+// PENDING LIST [ SYMBOL, ]
+
+const user1 = {
+  firstName: "Koushik",
+  lastName: "Chowdhury",
+  city: "Malda",
+  state: "West Bengal"
+}
+
+console.log(Object.keys(user1));
+console.log(Object.values(user1));
