99261e21e505acf6b6978ca728d897809fa481c7
Koushik Chowdhury <csakoushik@gmail.com>
2024-08-26 22:11:50 +0530

    modify

---
+++ loops/1.js
@@ -0,0 +1,31 @@
+// for (let index = 0; index < 10; index++) {
+//     const element = index;
+//     console.log(element);
+// }
+
+let myArray = ["koushik", "simran", "Chowdhury", "prosad", "koushik"];
+
+// for (let index = 0; index < myArray.length; index++) {
+//     const element = myArray[index];
+//     console.log(element);
+// }
+
+// for (const element of myArray) {
+//     console.log(element);
+
+// }
+myArray.forEach(function (item) {
+  console.log(item);
+});
+
+// foreach loop
+// const details = [
+//   {
+//     name: "koushi",
+//     lname: "chowdhury",
+//   },
+//   {
+//     name: "simran",
+//     lname: "prosad",
+//   },
+// ];
